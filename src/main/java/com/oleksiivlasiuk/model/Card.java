140d69163460450f2baeda48345ea67909955585
Oleksii Vlasiuk <vlasiuk.oleksii@gmail.com>
2024-05-20 19:22:37 +0200

    Fix code formatting

--- src/main/java/com/oleksiivlasiuk/model/Card.java
+++ src/main/java/com/oleksiivlasiuk/model/Card.java
@@ -4,11 +4,6 @@ public class Card {
     private final int rank;
     private final int suit;
 
-//    public Card(String rank, String suit) {
-//        this.rank = rank;
-//        this.suit = suit;
-//    }
-
     public Card(int rankHexValue, int suitHexValue) {
         this.rank = rankHexValue;
         this.suit = suitHexValue;
@@ -36,7 +31,6 @@ public class Card {
             case "J" -> 0xB;
             case "Q" -> 0xC;
             case "K" -> 0xD;
-            //case "A" -> 0xE;
             default -> 0xE;
         };
     }
@@ -55,7 +49,6 @@ public class Card {
             case 0xB -> "J";
             case 0xC -> "Q";
             case 0xD -> "K";
-            //case "A" -> 0xE;
             default -> "A";
         };
     }
@@ -65,7 +58,6 @@ public class Card {
             case "♠" -> 0x1;
             case "♥" -> 0x2;
             case "♦" -> 0x3;
-            //case "♣" -> 0x4;
             default -> 0x4;
         };
     }
@@ -75,52 +67,9 @@ public class Card {
             case 0x1 -> "♠";
             case 0x2 -> "♥";
             case 0x3 -> "♦";
-            //case "♣" -> 0x4;
             default -> "♣";
         };
     }
-
-//    public static int getCardRankValue(Card card) {
-//        return switch (card.getRank()) {
-//            case "2" -> 2;
-//            case "3" -> 3;
-//            case "4" -> 4;
-//            case "5" -> 5;
-//            case "6" -> 6;
-//            case "7" -> 7;
-//            case "8" -> 8;
-//            case "9" -> 9;
-//            case "10" -> 10;
-//            case "J" -> 11;
-//            case "Q" -> 12;
-//            case "K" -> 13;
-//            //case "A" -> 14;
-//            default -> 14;
-//        };
-//    }
-
-//    public static String getCardRankHexStringValue(Card card) {
-//        return switch (card.getRank()) {
-//            case "2" -> "2";
-//            case "3" -> "3";
-//            case "4" -> "4";
-//            case "5" -> "5";
-//            case "6" -> "6";
-//            case "7" -> "7";
-//            case "8" -> "8";
-//            case "9" -> "9";
-//            case "10" -> "A";
-//            case "J" -> "B";
-//            case "Q" -> "C";
-//            case "K" -> "D";
-//            //case "A" -> "E";
-//            default -> "E";
-//        };
-//    }
-
-//    public static int getCardRankHexValue(Card card) {
-//        return getRankHexValue(card.getRank());
-//    }
 
     @Override
     public String toString() {
